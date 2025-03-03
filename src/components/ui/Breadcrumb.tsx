f27b6cd27298c0b3dca095a617649005f04d4790
yosmisyael <yosmisyael7308@gmail.com>
2025-03-04 06:07:15 +0700

    feat: created breadcrumb component

---
+++ src/components/ui/Breadcrumb.tsx
@@ -0,0 +1,28 @@
+'use client'
+
+import { usePathname } from "next/navigation";
+import Link from "next/link";
+
+export default function Breadcrumb() {
+    const pathName = usePathname();
+    const paths = pathName.split("/").filter((item) => item !== "");
+    return (
+        <ul className="flex gap-5 text-mid-dark text-lg">
+            {
+                paths.map((path, index) => (
+                    <li className="inline-flex items-center capitalize" key={index}>
+                        <Link href={index !== paths.length - 1 ? `/${paths.slice(0, index + 1).join('/')}` : ''}>
+                            {path}
+                        </Link>
+                        {index !== paths.length - 1 && (
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5}
+                                 stroke="currentColor" className="size-4 ms-5">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5"/>
+                            </svg>
+                        )}
+                    </li>
+                ))
+            }
+        </ul>
+    )
+}

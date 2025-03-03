f183de81b4648cd192803e0015ffa1120fb26021
yosmisyael <yosmisyael7308@gmail.com>
2025-03-04 05:17:53 +0700

    feat: created dashboard page

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import DashboardLayout from "@/components/layouts/dashboard";
+
+export default function DashboardHome() {
+    return (
+        <DashboardLayout>
+            <h1>dashboard homepage here</h1>
+        </DashboardLayout>
+    );
+}

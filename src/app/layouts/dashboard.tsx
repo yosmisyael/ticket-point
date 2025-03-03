9c7e9a532c1e5e958190c40f7091aec86ef49dcd
yosmisyael <yosmisyael7308@gmail.com>
2025-03-03 21:41:11 +0700

    refactor: renamed layout for user dashboard

--- src/app/layouts/dashboard.tsx
+++ src/app/layouts/dashboard.tsx
@@ -1,16 +1,18 @@
-import Navbar from "@/app/components/navbar";
-import Sidebar from "@/app/components/sidebar";
+import Navbar from "@/components/navbar";
+import Sidebar from "@/components/sidebar";
+
 interface LayoutProps {
-    children: React.ReactNode; // Tipe untuk children
+    children: React.ReactNode;
 }
+
 export default function Layout({ children }: LayoutProps) {
     return (
         <div>
             <Navbar />
             <div className="flex flex-1 pt-16">
-                {/* Sidebar */}
+                {/* load sidebar */}
                 <Sidebar />
-                {/* Konten Dinamis */}
+                {/* main content */}
                 <main className="">
                     {children}
                 </main>

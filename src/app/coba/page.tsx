94350f35211aca680eb577c094e28c133de48c01
havidrosi05 <havidrosi05@gmail.com>
2025-03-03 16:51:54 +0700

    make layouts

---
+++ src/app/coba/page.tsx
@@ -0,0 +1,12 @@
+import Layout from "../layouts/page";
+
+
+export default function Coba() {
+    return (
+        <Layout>
+            <div className="flex-1 p-4 sm:ml-64">
+                <h1>Hallo Yosev</h1>
+            </div>
+        </Layout>
+    )
+}
---
+++ src/app/components/navbar.tsx
@@ -0,0 +1,66 @@
+import Image from "next/image";
+
+export default function Navbar() {
+    return (
+        <nav className="bg-white border-gray-200 dark:bg-gray-900">
+            <div className="max-w-screen-3xl flex flex-wrap items-center justify-between mx-auto px-12 p-4">
+                <a href="https://flowbite.com/" className="flex items-center space-x-3 rtl:space-x-reverse">
+                    {/* <Image
+                        src="https://flowbite.com/docs/images/logo.svg"
+                        className="h-8"
+                        alt="Flowbite Logo"
+                        width={32}
+                        height={32}
+                    /> */}
+                    <span className="self-center text-3xl font-semibold whitespace-nowrap dark:text-white"><span className="text-blue-600">Ticket</span> <span className="text-yellow-400">Point</span></span>
+                </a>
+                <div className="flex items-center md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+                    <button
+                        type="button"
+                        className="flex text-sm bg-gray-800 rounded-full md:me-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
+                        id="user-menu-button"
+                        aria-expanded="false"
+                        data-dropdown-toggle="user-dropdown"
+                        data-dropdown-placement="bottom"
+                    >
+                        <span className="sr-only">Open user menu</span>
+                        <Image
+                            className="w-8 h-8 rounded-full"
+                            src="/docs/images/people/profile-picture-3.jpg"
+                            alt="user photo"
+                            width={32}
+                            height={32}
+                        />
+                    </button>
+
+                    <div className="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow-sm dark:bg-gray-700 dark:divide-gray-600" id="user-dropdown">
+                        <div className="px-4 py-3">
+                            <span className="block text-sm text-gray-900 dark:text-white">Bonnie Green</span>
+                            <span className="block text-sm  text-gray-500 truncate dark:text-gray-400">name@flowbite.com</span>
+                        </div>
+                        <ul className="py-2" aria-labelledby="user-menu-button">
+                            <li>
+                                <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Dashboard</a>
+                            </li>
+                            <li>
+                                <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Settings</a>
+                            </li>
+                            <li>
+                                <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Earnings</a>
+                            </li>
+                            <li>
+                                <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Sign out</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <button data-collapse-toggle="navbar-user" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-user" aria-expanded="false">
+                        <span className="sr-only">Open main menu</span>
+                        <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15" />
+                        </svg>
+                    </button>
+                </div>
+            </div>
+        </nav>
+    )
+}

2b75de2957164e9591c7dd76a2e4a5cc455a1b87
yosmisyael <yosmisyael7308@gmail.com>
2025-03-04 10:42:13 +0700

    feat: created dashboard footer

---
+++ src/components/fragments/dashboard/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+        <footer className="col-span-4 fixed bottom-0 left-0 bg-light py-4 px-12 w-full flex justify-end">
+            <button className="bg-primary-mid text-white px-4 py-2 rounded-4xl">Save and Continue</button>
+        </footer>
+    )
+}

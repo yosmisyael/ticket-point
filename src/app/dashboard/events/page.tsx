e0416ec2bd631e3a58f7461c572d66e06c89b077
havidrosi05 <havidrosi05@gmail.com>
2025-03-05 21:54:15 +0700

    feat: add responsive event page and dashboard page

--- src/app/dashboard/events/page.tsx
+++ src/app/dashboard/events/page.tsx
@@ -1,7 +1,10 @@
+"use client";
 import Image from "next/image";
-import Breadcrumb from "@/components/ui/Breadcrumb";
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+import { useEffect } from "react";
 
-export default function Events() {
+export default function EventsPage() {
     const events = [
         {
             name: "TechVision 2025",
@@ -22,35 +25,107 @@ export default function Events() {
         return words.length > maxWords ? words.slice(0, maxWords).join(" ") + "..." : text;
     };
 
+    useEffect(() => {
+        flatpickr("#date", {
+            mode: "range",
+            dateFormat: "Y-m-d",
+        });
+    }, []);
+
     return (
-        <>
-            <div className="p-5">
-                <Breadcrumb />
-                <div className="mb-10">
-                    <h1 className="text-6xl font-semibold font-inter text-[var(--color-dark)]">Events </h1>
+        <div className="p-4 sm:p-6 md:p-10">
+            <div className="mb-6 md:mb-10">
+                <h1 className="mx-2 text-4xl sm:text-5xl md:text-6xl font-semibold font-inter text-[var(--color-dark)]">
+                    Events
+                </h1>
+            </div>
+
+            <div className="my-5 grid grid-cols-1 md:grid-cols-3 gap-4 mb-6 md:mb-10">
+                <form className="col-span-1 md:col-span-2 flex flex-col sm:flex-row gap-4">
+                    <div className="w-full sm:w-auto flex">
+                        <div>
+                            <label htmlFor="date" className="sr-only">Tanggal</label>
+                            <input
+                                type="text"
+                                id="date"
+                                placeholder="Select Date"
+                                className="w-full p-3 text-sm text-[var(--color-dark)] border border-[var(--color-mid-light)] rounded-lg bg-[var(--color-light)] focus:ring-[var(--color-primary-mid)] focus:border-[var(--color-primary-mid)] focus:outline-none"
+                            />
+                        </div>
+                        <div className="mx-2">
+                            <label htmlFor="default-search" className="sr-only">Search</label>
+                            <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                                <svg className="w-4 h-4 text-[var(--color-mid-dark)]" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
+                                </svg>
+                            </div>
+                            <input
+                                type="search"
+                                id="default-search"
+                                className="block w-full p-3 ps-10 mx-2 text-sm text-[var(--color-dark)] border border-[var(--color-mid-light)] rounded-lg bg-[var(--color-light)] focus:ring-[var(--color-primary-mid)] focus:border-[var(--color-primary-mid)] focus:outline-none"
+                                placeholder="Search Event..."
+                                required
+                            />
+                        </div>
+                        <button
+                            type="submit"
+                            className="text-[var(--color-light)] mx-2 bg-[var(--color-primary-mid)] hover:bg-[var(--color-primary-dark)] focus:ring-4 focus:outline-none focus:ring-[var(--color-primary-mid)] font-medium rounded-lg text-sm px-4 py-2"
+                        >
+                            Search
+                        </button>
+                    </div>
+                </form>
+                <div className="flex justify-end">
+                    <a
+                        href="#"
+                        className="flex w-fit items-center px-4 py-2 text-sm font-medium text-center rounded-lg text-[var(--color-light)] bg-[var(--color-primary-mid)] hover:bg-[var(--color-primary-dark)] focus:ring-4 focus:outline-none"
+                    >
+                        <svg className="w-6 h-6 text-[var(--color-light)] mr-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h14m-7 7V5" />
+                        </svg>
+
+                        Create Event
+                    </a>
                 </div>
-                <div className="grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1">
-                    {events.map((event, index) => (
-                        <div key={index} className="max-w-sm mx-2 my-2 bg-white border border-gray-200 rounded-lg shadow-sm">
+                <hr className="col-span-3 w-full border-[var(--color-mid-light)]" />
+            </div>
+
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+                {events.map((event, index) => (
+                    <div key={index} className="max-w-sm w-full bg-[var(--color-light)] border border-[var(--color-mid-light)] rounded-lg shadow-sm">
+                        <a href="#">
+                            <Image
+                                className="rounded-t-lg w-full h-48 object-cover"
+                                src={"/1.jpg"}
+                                width={500}
+                                height={500}
+                                alt={event.name}
+                            />
+                        </a>
+                        <div className="p-5">
                             <a href="#">
-                                <Image className="rounded-t-lg" src={"/1.jpg"} width={500} height={500} alt={event.name} />
+                                <h5 className="mb-2 text-2xl font-bold tracking-tight text-[var(--color-dark)] font-inter">
+                                    {event.name}
+                                </h5>
                             </a>
-                            <div className="p-5">
-                                <a href="#">
-                                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">{event.name}</h5>
-                                </a>
-                                <p className="mb-3 font-normal text-gray-700">{truncateWords(event.description)}</p>
-                                <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300">
+                            <p className="mb-3 font-normal text-[var(--color-mid-dark)] font-inter">
+                                {truncateWords(event.description)}
+                            </p>
+                            <div className="flex justify-end">
+                                <a
+                                    href="#"
+                                    className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-[var(--color-light)] bg-[var(--color-primary-mid)] hover:bg-[var(--color-primary-dark)] focus:ring-4 focus:outline-none"
+                                >
                                     Read more
                                     <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
                                     </svg>
                                 </a>
                             </div>
                         </div>
-                    ))}
-                </div>
+                    </div>
+                ))}
             </div>
-        </>
-    )
+        </div>
+    );
 }
--- src/app/dashboard/home/page.tsx
+++ src/app/dashboard/home/page.tsx
@@ -22,32 +22,36 @@ export default function DashboardHome() {
     };
 
     const name = "King Yosev";
+
     return (
-            <div className="p-10">
-                <div className="mb-10">
-                    <h1 className="mx-2 text-6xl font-semibold font-inter text-[var(--color-dark)]">Welcome, {name} </h1>
-                </div>
-                <div className="grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 ">
-                    {events.map((event, index) => (
-                        <div key={index} className="max-w-sm mx-2 my-2 bg-white border border-gray-200 rounded-lg shadow-sm">
+        <div className="p-10">
+            <div className="mb-10">
+                <h1 className="mx-2 text-6xl font-semibold font-inter text-[var(--color-dark)]">Welcome, {name} </h1>
+                {/* <p className="mx-2 text-xl my-5 ml-5 font-normal font-inter text-[var(--color-mid-dark)]">Rencanakan dan koordinasikan acara tersebut.</p> */}
+            </div>
+            <div className="grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 ">
+                {events.map((event, index) => (
+                    <div key={index} className="max-w-sm mx-2 my-2 bg-[var(--color-light)] border-[var(--color-mid-light)] rounded-lg shadow-sm ">
+                        <a href="#">
+                            <Image className="rounded-t-lg" src={"/1.jpg"} width={500} height={500} alt={event.name} />
+                        </a>
+                        <div className="p-5">
                             <a href="#">
-                                <Image className="rounded-t-lg" src={"/1.jpg"} width={500} height={500} alt={event.name} />
+                                <h5 className="mb-2 text-2xl font-bold tracking-tight text-[var(--color-dark)] font-inter">{event.name}</h5>
                             </a>
-                            <div className="p-5">
-                                <a href="#">
-                                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">{event.name}</h5>
-                                </a>
-                                <p className="mb-3 font-normal text-gray-700">{truncateWords(event.description)}</p>
-                                <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300">
+                            <p className="mb-3 font-normal text-[var(--color-mid-dark)] font-inter">{truncateWords(event.description)}</p>
+                            <div className="flex justify-end">
+                                <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-[var(--color-light)] bg-[var(--color-primary-mid)] hover:bg-[var(--color-primary-dark)] focus:ring-4 focus:outline-none ">
                                     Read more
                                     <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
                                     </svg>
                                 </a>
                             </div>
                         </div>
-                    ))}
-                </div>
+                    </div>
+                ))}
             </div>
+        </div>
     );
 }

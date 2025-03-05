54609647ef0a3edc47f8413cdfc4ad72958c055e
yosmisyael <yosmisyael7308@gmail.com>
2025-03-06 06:11:09 +0700

    feat: created navbar fragments

--- src/components/fragments/dashboard/Navbar.tsx
+++ src/components/fragments/dashboard/Navbar.tsx
@@ -1,12 +1,17 @@
-import Image from "next/image";
 import { NextFont } from "next/dist/compiled/@next/font";
 import { Poppins } from "next/font/google";
 import Link from "next/link";
+import UserMenuButton from "@/components/ui/UserMenuButton";
 
 const poppins: NextFont = Poppins({
     weight: ['200', '400', '800'],
     subsets: ['latin'],
 });
+
+const dummyUser = {
+    name: 'Havid Rosihandanuu',
+    email: 'mangaped@king.com',
+}
 
 export default function Navbar() {
     return (
@@ -16,50 +21,7 @@ export default function Navbar() {
                     <span className="self-center text-3xl font-bold whitespace-nowrap"><span className="text-primary-dark">Ticket</span><span className="text-alternative-mid">Point</span></span>
                 </Link>
                 <div className="flex items-center md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-                    <button
-                        type="button"
-                        className="flex text-sm bg-gray-800 rounded-full md:me-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
-                        id="user-menu-button"
-                        aria-expanded="false"
-                        data-dropdown-toggle="user-dropdown"
-                        data-dropdown-placement="bottom"
-                    >
-                        <span className="sr-only">Open user menu</span>
-                        <Image
-                            className="w-8 h-8 rounded-full"
-                            src="/docs/images/people/profile-picture-3.jpg"
-                            alt="user photo"
-                            width={32}
-                            height={32}
-                        />
-                    </button>
-
-                    <div className="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow-sm dark:bg-gray-700 dark:divide-gray-600" id="user-dropdown">
-                        <div className="px-4 py-3">
-                            <span className="block text-sm text-gray-900 dark:text-white">Bonnie Green</span>
-                            <span className="block text-sm  text-gray-500 truncate dark:text-gray-400">name@flowbite.com</span>
-                        </div>
-                        <ul className="py-2" aria-labelledby="user-menu-button">
-                            <li>
-                                <Link href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Dashboard</Link>
-                            </li>
-                            <li>
-                                <Link href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Settings</Link>
-                            </li>
-                            <li>
-                                <Link href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Earnings</Link>
-                            </li>
-                            <li>
-                                <Link href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Sign out</Link>
-                            </li>
-                        </ul>
-                    </div>
-                    <button data-collapse-toggle="navbar-user" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-user" aria-expanded="false">
-                        <span className="sr-only">Open main menu</span>
-                        <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15" />
-                        </svg>
-                    </button>
+                    <UserMenuButton user={dummyUser} />
                 </div>
             </div>
         </nav>

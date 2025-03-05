436a159752706a6a6b934fe809d4dc5bb293842f
yosmisyael <yosmisyael7308@gmail.com>
2025-03-06 06:12:55 +0700

    fixed: fixed attribute name errro

--- src/app/dashboard/events/page.tsx
+++ src/app/dashboard/events/page.tsx
@@ -73,7 +73,7 @@ export default function EventsPage() {
                     <div className="relative w-full">
                         <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
                             <svg className="w-5 h-5 text-[var(--color-mid-dark)]" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                                <path stroke="currentColor" strokeLinecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
+                                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
                             </svg>
                         </div>
                         <label htmlFor="default-search" className="sr-only">Search</label>

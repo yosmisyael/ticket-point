15fadbca5db0b676381b473ebc4832ff1e85d3ba
yosmisyael <yosmisyael7308@gmail.com>
2025-03-06 10:30:55 +0700

    feat: created email and password settings page

---
+++ src/app/dashboard/settings/email/page.tsx
@@ -0,0 +1,7 @@
+export default function Email() {
+    return (
+        <div className="p-4 sm:p-6 md:p-10 text-dark">
+            <h1>THIS IS EMAIL CHANGE PAGE</h1>
+        </div>
+    )
+}
---
+++ src/app/dashboard/settings/password/page.tsx
@@ -0,0 +1,7 @@
+export default function Password() {
+    return (
+        <div className="p-4 sm:p-6 md:p-10 text-dark">
+            <h1>THIS IS Password CHANGE PAGE</h1>
+        </div>
+    )
+}

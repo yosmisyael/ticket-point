578a4f2d4f4e5142e3e99546cbce7747a9ef1f47
yosmisyael <yosmisyael7308@gmail.com>
2025-03-06 10:26:39 +0700

    feat: updated dynamic sidebar

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,20 +1,24 @@
+"use client";
+
 import Navbar from "@/components/fragments/dashboard/Navbar";
 import Sidebar from "@/components/fragments/dashboard/Sidebar";
+import SidebarSettings from "@/components/fragments/dashboard/settings/Sidebar";
 
+import { usePathname } from "next/navigation";
 interface LayoutProps {
     children: React.ReactNode;
 }
 
 export default function DashboardLayout({ children }: LayoutProps) {
+    const pathname = usePathname();
+
     return (
         <>
             <div className="grid grid-cols-5">
-                <Navbar />
-                {/* load sidebar */}
-                <Sidebar />
-                {/* main content */}
-                <main className="col-span-4 max-h-screen overflow-y-scroll">
-                    {children}
+                <Navbar/>
+                { pathname.startsWith("/dashboard/settings") ? <SidebarSettings/> : <Sidebar/>}
+                <main className={`col-span-4 max-h-screen ${pathname.startsWith("/dashboard/settings") ? "" : "overflow-y-scroll"}`}>
+                    { children }
                 </main>
             </div>
         </>

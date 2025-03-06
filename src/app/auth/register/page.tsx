35cf3c427ad3e90e75ea8ebb115d485e393b86d8
yosmisyael <yosmisyael7308@gmail.com>
2025-03-06 22:12:38 +0700

    fix: fixed link href value

--- src/app/auth/register/page.tsx
+++ src/app/auth/register/page.tsx
@@ -27,7 +27,7 @@ export default function Register() {
             </Link>
             <div className="flex gap-4">
               <Link
-                href="/login"
+                href="/auth/login"
                 className="inline-flex items-center px-4 py-2 border border-gray-300 rounded-full text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 transition-all duration-300"
               >
                 <User className="h-5 w-5 mr-2" />
@@ -139,7 +139,7 @@ export default function Register() {
                 <p className="text-sm text-gray-600">
                   Sudah punya akun?{" "}
                   <Link
-                    href="/login"
+                    href="/auth/login"
                     className="font-medium text-yellow-400 hover:text-yellow-500 transition-all duration-300"
                   >
                     Masuk Disini
